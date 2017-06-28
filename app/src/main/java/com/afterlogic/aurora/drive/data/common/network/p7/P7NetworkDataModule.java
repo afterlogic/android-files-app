05cea9273b7ebc1874122e99dcd7b49c8b217390
Aleksandr Tcikin <mail@sunnydaydev.me>
2017-06-28 16:04:35 +0300

    Update android studio and libraries.

--- app/src/main/java/com/afterlogic/aurora/drive/data/common/network/p7/P7NetworkDataModule.java
+++ app/src/main/java/com/afterlogic/aurora/drive/data/common/network/p7/P7NetworkDataModule.java
@@ -9,13 +9,13 @@ import com.afterlogic.aurora.drive.data.common.network.util.IgnoreSerealizationE
 import com.afterlogic.aurora.drive.data.model.project7.ApiResponseP7;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
 import dagger.Module;
 import dagger.Provides;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
